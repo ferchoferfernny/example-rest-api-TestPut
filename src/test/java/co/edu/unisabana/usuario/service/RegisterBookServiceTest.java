42666bf952191a83ee81c295890c6321fd69acea
rximenars <ximis.ximena@gmail,com>
2022-09-10 17:49:30 -0500

    Se agregan tests de service actualizados.

--- src/test/java/co/edu/unisabana/usuario/service/RegisterBookServiceTest.java
+++ src/test/java/co/edu/unisabana/usuario/service/RegisterBookServiceTest.java
@@ -2,6 +2,7 @@ package co.edu.unisabana.usuario.service;
 
 import co.edu.unisabana.usuario.service.library.RegisterBookLibrary;
 import co.edu.unisabana.usuario.service.library.model.Book;
+import co.edu.unisabana.usuario.service.library.model.CategoryBook;
 import co.edu.unisabana.usuario.service.library.port.AddBookPort;
 import co.edu.unisabana.usuario.service.library.port.RegisterBookPort;
 import org.junit.jupiter.api.Test;
@@ -29,22 +30,18 @@ public class RegisterBookServiceTest
     @Test
     public void Given_nameExists_When_RegisterBook_Then_addBook()
     {
-        Book book = new Book();
-        book.setName("Alicia en el País de las Maravillas");
-        Mockito.when(addBookPort.validateExistsBook(book.getName)).thenReturn(true);
-        int exists =service.addBook(book);
-
-        Mockito.verify(addBookPort).validateExistsBook(book.getName());
+        Book book = new Book("Alicia en el Pais ", 1999, "Julio Berne", false,CategoryBook.SOFT_COVER);
+        Mockito.when(searchBookPort.validateExistsBook(book.getName())).thenReturn(true);
+        int exists =service.registerBook(book);
+        Mockito.verify(searchBookPort).validateExistsBook(book.getName());
         assertEquals(1,exists);
     }
 
     public void Given_nameNotExists_When_RegisterBook_Then_registerBook() {
-        Book book = new Book();
-        book.setName("A través de mi ventana");
-        Mockito.when(registerBookPort.validateExiistsBook(book.getName())).thenReturn(true);
+        Book book = new Book("Alicia en el Pais ", 1999, "Julio Berne", false, CategoryBook.SOFT_COVER);
+        Mockito.when(searchBookPort.validateExistsBook(book.getName())).thenReturn(false);
         int exists =service.registerBook(book);
-
-        Mockito.verify(registerBookPort).validateExistsBook(book.getName());
+        Mockito.verify(searchBookPort).validateExistsBook(book.getName());
         assertEquals(2,exists);
     }
 
@@ -79,7 +76,8 @@ public class RegisterBookServiceTest
 
     @Test
     public void Give_dontSendCorrectIinformantion_When_RegisterBook_Then_throwIllegalArgument() {
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () ->
+        {
             service.registerBook(new Book());
         });
     }
